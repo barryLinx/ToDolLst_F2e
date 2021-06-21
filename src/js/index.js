9519bb6e504274a90d0864d7653469f2e5b5630d
Bluce <a1019521a@gmail.com>
2021-06-21 14:17:51 +0800

    Dom操作 type:node 型別 | 新增方法(function) spanTitleDom() , delTitleDom() ,inputcheck_change() | 用到的屬型: el.parentNode nextElementSibling |事件: replaceChild()，insertAdjacentHTML()， classList.add(class,classs,class)，

--- src/js/index.js
+++ src/js/index.js
@@ -15,23 +15,54 @@ const navLinkDom = [...document.querySelectorAll('.nav-link')];
 const fromCancel = document.querySelector('.from-cancel');
 const fromAdd = document.querySelector('.from-add')
 
-const inputCheckDom =[...document.querySelectorAll('.hascheck')];
+const inputCheckDom = [...document.querySelectorAll('.hascheck')];
 let alldata = [];
 
-console.log(inputCheckDom);
-function inputcheck_click (){
-  inputCheckDom.forEach(el=>{
-    // el.addEventListener('click',()=>{
-    //   el.appendChild(`<del class="text-gray-200">
-    //   <span class="ff-Rob fs-3 todo-title">
-    //         已經變動
-    //   </span>
-    //   </del>`);
-    //   console.log(el);  
-    // })
-    
+
+function delTitleDom() {
+  // let str = ` <del class="text-gray-200">
+  //          <span class="ff-Rob fs-3 todo-title">
+  //             Type Something Here…  
+  //          </span>
+  //       </del>`;
+  let title = document.createTextNode("New Something Here… ");
+  let del_dom = document.createElement('del');
+  del_dom.classList.add('text-gray-200');
+  let span_dom = document.createElement('span');
+  span_dom.classList.add('ff-Rob', 'fs-3', 'todo-title');
+  span_dom.appendChild(title);
+  del_dom.appendChild(span_dom);
+  console.log(del_dom);
+  return del_dom;
+}
+
+function spanTitleDom(){
+  let title = document.createTextNode('Type Something Here…');
+  let sapn = document.createElement('span');
+  sapn.classList.add('ff-Rob','fs-3', 'todo-title');
+  sapn.appendChild(title);
+  return sapn;
+}
+
+
+function inputcheck_change() {
+  inputCheckDom.forEach(el => {
+    el.addEventListener('change', (event) => {
+      //el.insertAdjacentHTML("afterend", delHtml());
+      //console.log(el.parentNode);
+      console.log(event.currentTarget.checked);
+      let elParentNode = el.parentNode;
+      let elNextEleSib = el.nextElementSibling;
+      //console.log(el.nextElementSibling);
+      if(event.currentTarget.checked){
+        elParentNode.replaceChild(delTitleDom(),elNextEleSib);
+      }else{
+        elParentNode.replaceChild(spanTitleDom(),elNextEleSib); 
+      }         
+    })
   })
 }
+
 
 
 function hasActive(element) {
@@ -55,91 +86,91 @@ function filter_Event(param = '') {
 
 //display products UI
 class UI {
-  editUI(dataObj) {
-    const editSection = document.querySelector('.edit');
-    let str = `<div class="w-50 d-flex px-4 py-3 bg-light">
-    <div class="me-auto d-inline-block">
-      <input type="checkbox" class="w-6 h-6 me-3">
-      <span class="ff-Rob fs-3">Type Something Here…</span>
-    </div>
-    <div class="d-inline-block fs-3">
-      <a href="" class="markStar text-black text-decoration-no">
-        <i class="far fa-star"></i>
-      </a>
-      <a href="" class="edit-pencil-btn text-primary text-decoration-no">
-        <i class="fas fa-pencil-alt"></i>
-      </a>
-    </div>
-  </div>`;
-    editSection.innerHTML = str;
-  }
-  editContent(dataObj) {
-    const editcontent = document.querySelector('.edit-content');
+  // editUI(dataObj) {
+  //   const editSection = document.querySelector('.edit');
+  //   let str = `<div class="w-50 d-flex px-4 py-3 bg-light">
+  //   <div class="me-auto d-inline-block">
+  //     <input type="checkbox" class="w-6 h-6 me-3">
+  //     <span class="ff-Rob fs-3">Type Something Here…</span>
+  //   </div>
+  //   <div class="d-inline-block fs-3">
+  //     <a href="" class="markStar text-black text-decoration-no">
+  //       <i class="far fa-star"></i>
+  //     </a>
+  //     <a href="" class="edit-pencil-btn text-primary text-decoration-no">
+  //       <i class="fas fa-pencil-alt"></i>
+  //     </a>
+  //   </div>
+  // </div>`;
+  //   editSection.innerHTML = str;
+  // }
+  // editContent(dataObj) {
+  //   const editcontent = document.querySelector('.edit-content');
 
-    let str = `<div class="d-flex flex-column w-50">
-    <form class="needvail bg-light p-5">
-      <div class="form-control mb-5">
-        <div class="ms-4">
-          <label for="yeardate" class="ps-3">
-            <span><i class="far fa-calendar-alt fs-4"></i>
-            </span>
-            <span class="ff-Rob fs-4 ps-1">Deadline</span>
-          </label>
-        </div>
-        <div class="ps-5 ms-4">
-          <input id="yeardate" class="border-0 py-1 px-2" placeholder="yyyy/mm/dd">
-          <input id="timestamp" class="border-0 py-1 px-2" placeholder="hh:mm">
-        </div>
-      </div>
-      <div class="form-control mb-5">
-        <div class="ms-4">
-          <label for="fupload" class="ps-3">
-            <span> <i class="far fa-file fs-4"></i></span>
-            <span class="ms-2 ff-Rob fs-4">File</span>
-          </label>
-        </div>
-        <div class="ps-5 ms-4 d-flex">
-          <div class="me-3">
-            <p class="m-0 fs-6 ff-Rob">
-              20180514.zip
-            </p>
-            <p class="m-0 text-dark ff-Rob">uploaded yesterday</p>
-          </div>
-          <label for="fupload" class="bg-gray px-3 py-2 c-p d-flex align-items-center">
-            <i class="fas fa-plus fs-5 text-white"></i>
-          </label>
-          <input id="fupload" type="file" hidden>
-        </div>
-      </div>
+  //   let str = `<div class="d-flex flex-column w-50">
+  //   <form class="needvail bg-light p-5">
+  //     <div class="form-control mb-5">
+  //       <div class="ms-4">
+  //         <label for="yeardate" class="ps-3">
+  //           <span><i class="far fa-calendar-alt fs-4"></i>
+  //           </span>
+  //           <span class="ff-Rob fs-4 ps-1">Deadline</span>
+  //         </label>
+  //       </div>
+  //       <div class="ps-5 ms-4">
+  //         <input id="yeardate" class="border-0 py-1 px-2" placeholder="yyyy/mm/dd">
+  //         <input id="timestamp" class="border-0 py-1 px-2" placeholder="hh:mm">
+  //       </div>
+  //     </div>
+  //     <div class="form-control mb-5">
+  //       <div class="ms-4">
+  //         <label for="fupload" class="ps-3">
+  //           <span> <i class="far fa-file fs-4"></i></span>
+  //           <span class="ms-2 ff-Rob fs-4">File</span>
+  //         </label>
+  //       </div>
+  //       <div class="ps-5 ms-4 d-flex">
+  //         <div class="me-3">
+  //           <p class="m-0 fs-6 ff-Rob">
+  //             20180514.zip
+  //           </p>
+  //           <p class="m-0 text-dark ff-Rob">uploaded yesterday</p>
+  //         </div>
+  //         <label for="fupload" class="bg-gray px-3 py-2 c-p d-flex align-items-center">
+  //           <i class="fas fa-plus fs-5 text-white"></i>
+  //         </label>
+  //         <input id="fupload" type="file" hidden>
+  //       </div>
+  //     </div>
 
-      <div class="form-control mb-5">
-        <div class="ms-4">
-          <label for="message" class="ps-3">
-            <span>
-              <i class="far fa-comment-dots fs-4"></i>
-            </span>
-            <span class="ms-2 ff-Rob fs-4">Comment</span>
-          </label>
-        </div>
-        <div class="ps-5 ms-4">
-          <textarea id="message" rows="5" cols="42"></textarea>
-        </div>
-      </div>
-    </form>
-    <div class="footer d-flex justify-content-center">
-      <button class="w-50 fs-3 p-3 border-0 bg-white text-danger ff-Rob">
-        <i class="fas fa-times me-3"></i>
-        Cancel
-      </button>
-      <button class="w-50 fs-3 p-3 border-0 bg-primary text-white ff-Rob">
-        <i class="fas fa-plus me-3"></i>
-        Add Task
-      </button>
-    </div>
-  </div>`;
+  //     <div class="form-control mb-5">
+  //       <div class="ms-4">
+  //         <label for="message" class="ps-3">
+  //           <span>
+  //             <i class="far fa-comment-dots fs-4"></i>
+  //           </span>
+  //           <span class="ms-2 ff-Rob fs-4">Comment</span>
+  //         </label>
+  //       </div>
+  //       <div class="ps-5 ms-4">
+  //         <textarea id="message" rows="5" cols="42"></textarea>
+  //       </div>
+  //     </div>
+  //   </form>
+  //   <div class="footer d-flex justify-content-center">
+  //     <button class="w-50 fs-3 p-3 border-0 bg-white text-danger ff-Rob">
+  //       <i class="fas fa-times me-3"></i>
+  //       Cancel
+  //     </button>
+  //     <button class="w-50 fs-3 p-3 border-0 bg-primary text-white ff-Rob">
+  //       <i class="fas fa-plus me-3"></i>
+  //       Add Task
+  //     </button>
+  //   </div>
+  // </div>`;
 
-    editcontent.innerHTML = str;
-  }
+  //   editcontent.innerHTML = str;
+  // }
   addTaskBtn_hide_UI() {
     //let style = getComputedStyle(addTaskBtn, 'display');
     addTaskBtn.classList.add('d-none');
@@ -229,7 +260,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const ui = new UI();
   ui.setup();
   //navLink_click();
-  inputcheck_click()
+  inputcheck_change();
 })
 
 
