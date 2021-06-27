14cd87870605b82684b93585286c6dac54441cb5
Bluce <a1019521a@gmail.com>
2021-06-27 20:50:03 +0800

    clean code | element 用forEach迴圈 和 物件解構 { ...element , pops : document.getElementById(item) } |

--- src/js/index.js
+++ src/js/index.js
@@ -6,30 +6,49 @@ import { faFile, faCommentDots, faCalendarAlt } from '@fortawesome/fontawesome-f
 
 library.add(faPlus, faPencilAlt, faFile, faStar, faCommentDots, faCalendarAlt)
 
-// const element = {}; 進行clean
-const addInput = document.querySelector('.addInput');
-const addTaskBtn = document.querySelector('#addTaskBtn');
+// /進行clean code
+// const addInput = document.querySelector('#addInput');
+// const addTaskBtn = document.querySelector('#addTaskBtn');
 
 //const editCheck = document.querySelector('.editCheck'); //勾選是否完成
-const editTitle = document.querySelector('.editTitle'); //編輯 新增 標題
+// const editTitle = document.querySelector('#editTitle'); //編輯 新增 標題
 
-const editStar = document.querySelector('#editStar'); // 編輯 新增 重要
-const editBtn = document.querySelector('.editBtn'); //  編輯 
-const editContent = document.querySelector('.edit-content');
-const editFileName = document.querySelector('#editFileName');
-const editFileTime = document.querySelector('#editFileTime');
-const editDateTime = document.querySelector('#editDateTime');
-const editMessage = document.querySelector('#editMessage');
+// const editStar = document.querySelector('#editStar'); // 編輯 新增 重要
+// const editBtn = document.querySelector('#editBtn'); //  編輯 
+// const editContent = document.querySelector('#editContent');
+// const editFileName = document.querySelector('#editFileName');
+// const editFileTime = document.querySelector('#editFileTime');
+// const editDateTime = document.querySelector('#editDateTime');
+// const editMessage = document.querySelector('#editMessage');
 
 const navLinkDom = [...document.querySelectorAll('.nav-link')];
-const fupload = document.querySelector('#fupload');
-const formCancel = document.querySelector('.form-cancel');
-const formAdd = document.querySelector('.form-add');
-const formEdit = document.querySelector('#formEdit');
-//const formEdit = document.forms["formEdit"];
-const taskList = document.querySelector('#TaskList');
-const Total = document.querySelector('#Total');
+// const fupload = document.querySelector('#fupload');
+// const formCancel = document.querySelector('#formCancel');
+// const formAdd = document.querySelector('#formAdd');
+// //const formEdit = document.querySelector('#formEdit');
+// //const formEdit = document.forms["formEdit"];
+// const TaskList = document.querySelector('#TaskList');
+// const Total = document.querySelector('#Total');
 
+let element = {};//進行clean code
+
+['addInput', 'addTaskBtn',
+  'editTitle', 'editStar', 'editBtn',
+  'editContent', 'editFileName',
+  'editFileTime', 'editDateTime',
+  'editMessage', 'fupload', 'formCancel',
+  'formAdd', 'TaskList', 'Total']
+  .forEach(item => element = {
+    ...element,
+    [item]: document.getElementById(item)
+  });
+const { addInput, addTaskBtn, editTitle,
+  editStar, editBtn,
+  editContent, editFileName,
+  editFileTime, editDateTime,
+  editMessage, fupload,
+  formCancel, formAdd,
+  TaskList, Total } = element;
 
 // const element = {}; 進行clean
 
@@ -69,20 +88,7 @@ function hasActive(element) {
 /**
  * 將mark放在最上面
  */
-// function filterData_Event() {
 
-//   let mark = alldata.filter(d => {
-//     return d.reminder == true;
-//   });
-
-//   let nomark = alldata.filter(d => {
-//     return d.reminder !== true;
-//   });
-
-//   //alldata.length = 0;
-//   alldata = mark.concat(nomark);
-//   console.log("alldata", alldata);
-// }
 
 function getSelectData(id) {
   //console.log("getSelectData", alldata);
@@ -106,7 +112,7 @@ function sortData_Event(data) {
   //console.log(nomark);
   //alldata.length = 0;
   alldata = [...mark, ...nomark];
-  Total.textContent=`${alldata.length} tasks left`;
+  Total.textContent = `${alldata.length} tasks left`;
   //index.jsconsole.log("filterData_Event:", alldata);
 }
 
@@ -118,32 +124,35 @@ function filterData_Event(status) {
 
 
 
+
+
 //display products UI
 class UI {
-  delTitleDom(t) {
-    // let str = ` <del class="text-gray-200">
-    //          <span class="ff-Rob fs-3 todo-title">
-    //             Type Something Here…  
-    //          </span>
-    //       </del>`;
-    let title = document.createTextNode(t);
-    let del_dom = document.createElement('del');
-    del_dom.classList.add('text-gray-200');
-    let span_dom = document.createElement('span');
-    span_dom.classList.add('ff-Rob', 'fs-3', 'todo-title');
-    span_dom.appendChild(title);
-    del_dom.appendChild(span_dom);
-    console.log(del_dom);
-    return del_dom;
-  }
+  // delTitleDom(t) {
+  //   // let str = ` <del class="text-gray-200">
+  //   //          <span class="ff-Rob fs-3 todo-title">
+  //   //             Type Something Here…  
+  //   //          </span>
+  //   //       </del>`;
+  //   let title = document.createTextNode(t);
+  //   let del_dom = document.createElement('del');
+  //   del_dom.classList.add('text-gray-200');
+  //   let span_dom = document.createElement('span');
+  //   span_dom.classList.add('ff-Rob', 'fs-3', 'todo-title');
+  //   span_dom.appendChild(title);
+  //   del_dom.appendChild(span_dom);
+  //   console.log(del_dom);
+  //   return del_dom;
+  // }
 
-  spanTitleDom(t) {
-    let title = document.createTextNode(t);
-    let sapn = document.createElement('span');
-    sapn.classList.add('ff-Rob', 'fs-3');
-    sapn.appendChild(title);
-    return sapn;
-  }
+  // spanTitleDom(t) {
+  //   let title = document.createTextNode(t);
+  //   let sapn = document.createElement('span');
+  //   sapn.classList.add('ff-Rob', 'fs-3');
+  //   sapn.appendChild(title);
+  //   return sapn;
+  // }
+
   /**
    * 改變 標題 樣式 加上 <del></del>
    */
@@ -189,24 +198,25 @@ class UI {
       //console.log(el);
       el.addEventListener("change", (e) => {
         //e.preventDefault();
-        //console.log(el.dataset.id);
+        console.log(e.target);
         let seldata = getSelectData(el.dataset.id);//選出要改變的data
-        console.log((e.currentTarget.checked));
+        //console.log((e.currentTarget.checked));
         //console.log(el.previousElementSibling);
         //putData()
         //console.log(el.previousElementSibling.children[0]);
         let fdata = removeData(el.dataset.id);//從集合移除data，
         //console.log(fdata);
 
-        // alldata 更改處理
+        // alldata 更改處理      
         if (e.currentTarget.checked) {
           seldata.reminder = true;
-          fdata.unshift(seldata);  //再加入最前面
+          fdata.unshift(seldata);  //加入最前面
         } else {
           seldata.reminder = false; //沒有標記，不用再加入最前面
         }
-
         alldata = fdata;
+        //alldata = Object.assign({},fdata);
+        //console.log(alldata);
         //做Http put
         putData(seldata);
         this.listUI(alldata);
@@ -229,21 +239,16 @@ class UI {
         this.editContent_show_UI();
         //el.dataset.id;
         let sdata = getSelectData(el.dataset.id);
-        let {id,status,title,reminder,comment,dateTime,upLoadFileName} = sdata;
+
+        let { title, reminder, comment, dateTime } = sdata;
+
         editTitle.value = title;
         editStar.checked = reminder;
         editDateTime.value = dateTime;
         editMessage.value = comment;
-        
-        editObj.id = id;
-        editObj.title = title;
-        editObj.status = status;
-        editObj.reminder = reminder;
-        editObj.comment= comment;
-        editObj.dateTime = dateTime;
-        editObj.upLoadFileName.name = upLoadFileName.name;
-        editObj.upLoadFileName.lastModifiedDate = upLoadFileName.lastModifiedDate;
-       
+        //editObj = sdata; 
+        editObj = Object.assign({}, sdata); //clean code
+
         //scroll 卷軸向上
         window.scrollTo({
           top: 0,
@@ -295,22 +300,22 @@ class UI {
         console.log(status);
         switch (status) {
           case 'Progress':
-            fdata = filterData_Event(status);           
-            Total.textContent=`${fdata.length} tasks left`;
+            fdata = filterData_Event(status);
+            Total.textContent = `${fdata.length} tasks left`;
             this.listUI(fdata);
             // restart render UI 
             //filter_Event('Progress')
             break;
           case 'Completed':
-            fdata = filterData_Event(status);            
-            Total.textContent=`${fdata.length} tasks left`;
+            fdata = filterData_Event(status);
+            Total.textContent = `${fdata.length} tasks left`;
             this.listUI(fdata);
             // restart render UI
             //filter_Event('Completed')
             break;
           default:
             sortData_Event(alldata);
-            Total.textContent=`${fdata.length} tasks left`;
+            Total.textContent = `${fdata.length} tasks left`;
             this.listUI(alldata);
             //filterData_Event();
             break;
@@ -353,9 +358,9 @@ class UI {
   </div>
 </li>`;
     })
-    taskList.innerHTML = str;
-    this.inputcheck_change(); //不知為何要放這，難道是記憶體問題
-    this.markStar_click(); //不知為何要放這，難道是記憶體問題
+    TaskList.innerHTML = str;
+     this.inputcheck_change(); //不知為何要放這，難道是記憶體問題
+     this.markStar_click(); //不知為何要放這，難道是記憶體問題
     this.editpencel(); //不知為何要放這，難道是記憶體問題
   }
 
@@ -446,10 +451,10 @@ class UI {
       this.editBtn_hide_UI();
       this.editContent_hide_UI();
       addInput.value = '';
-      
+
       editTitle.value = ''
-      editDateTime.value='';
-      editMessage.value='';
+      editDateTime.value = '';
+      editMessage.value = '';
     })
 
     formAdd.addEventListener('click', (e) => {
@@ -460,12 +465,10 @@ class UI {
       // console.log();   
       //addedit = 'Add';    
       formAddEdit_click();
-     
+
       //console.log(editObj);
     })
     //this.editpencel();
-
-
   }
 };
 
@@ -475,12 +478,12 @@ function formAddEdit_click() {
   console.log(editObj);
 
   switch (addedit) {
-    case 'Save':      
+    case 'Save':
       console.log('PUT');
       putData(editObj);
       break;
-    case 'Add':  
-      console.log('POST');   
+    case 'Add':
+      console.log('POST');
       postData(editObj);
       break;
     default:
@@ -519,13 +522,16 @@ function sendHttpReq(method, url, data) {
 
 async function getData() {
   let data;
-  await sendHttpReq('GET', url).then(res => {
-    console.log(res)
-    data = res;
-  });
+  // await sendHttpReq('GET', url).then(res => {
+  //   console.log(res)
+  //   data = res;
+  // });
+  data = await sendHttpReq('GET', url);
+  //console.log('data',data);
   return data;
 }
 
+// 有 async /await 不用 .then(res=>{})
 async function postData(data) {
   let ui = new UI();
   await sendHttpReq('POST', url, data).then(res => {
@@ -544,6 +550,7 @@ async function postData(data) {
   });
 }
 
+// 有 async /await 不用 .then(res=>{})
 async function putData(data) {
   let ui = new UI();
   await sendHttpReq('PUT', url + `/${data.id}`, data).then(res => {
@@ -551,8 +558,9 @@ async function putData(data) {
     ui.addTaskBtn_show_UI();
     ui.editBtn_hide_UI();
     ui.editContent_hide_UI();
-  }).then(()=>{
-    getData().then(data =>{
+  }).then(() => {
+
+    getData().then(data => {
       sortData_Event(data);//排序
       ui.listUI(alldata);
     })
@@ -563,21 +571,30 @@ async function putData(data) {
 /**
  * init
  */
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
   const ui = new UI();
   //const event = new Event();
   //const dto = new DTO();
   ui.setup();
 
-  getData().then(data => {
-    //console.log(data);
-    //alldata = data;
-    sortData_Event(data);
-    ui.listUI(alldata);
-    //ui.editpencel();
-    //ui.inputcheck_change();
-    //ui.markStar_click();
-  })
+
+  let data = await getData(); // clean promise
+  sortData_Event(data);
+  ui.listUI(alldata);
+  // ui.editpencel(); // 記憶體執行環境問題
+  // ui.inputcheck_change(); // 記憶體執行環境問題
+  // ui.markStar_click(); // 記憶體執行環境問題
+
+  console.log("DOMContentLoaded", data);
+  // getData().then(data => {
+  //   //console.log(data);
+  //   //alldata = data;
+  //   sortData_Event(data);
+  //   ui.listUI(alldata);
+  //   //ui.editpencel();
+  //   //ui.inputcheck_change();
+  //   //ui.markStar_click();
+  // })
 
   //navLink_click();
   //  ui.inputcheck_change();
